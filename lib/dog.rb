af597680ec74270ac44a74f9621f3935c4d02af0
maxjacobzander <maxjacobzander@gmail.com>
2020-08-25 10:46:16 -0400

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,14 @@
 # Your code goes here!
+class Dog
+    def name=(dog_name)
+        @dog_name_is = dog_name
+    end
+
+    def name
+        @dog_name_is
+    end
+
+    def bark
+        puts "woof!"
+    end
+end
