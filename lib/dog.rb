0940fe74ee9343fbf021e0e8ac10a15726534765
selam lemma <oneiconforlife26@gmail.com>
2020-08-31 11:58:38 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,14 @@
 # Your code goes here!
+class Dog
+  def name=(fido)
+    @dog_name = fido
+  end
+  def name
+    @dog_name
+  end
+  def bark
+  puts "woof!"
+end
+end
+fido = Dog.new
+fido.bark 
